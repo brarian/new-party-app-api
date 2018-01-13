65721e4431e630b17af6648afb0555cab56184c6
brarian <b.namonywa@gmail.com>
2018-01-13 10:36:33 -0600

    CORS middleware added.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+//cors
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 const PORT = process.env.PORT || 8080;
+
 
 app.get('/', (req, res) => {
  res.json({message: ok});
